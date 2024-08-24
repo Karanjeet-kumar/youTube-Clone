110339b6d022c355e0b1f3bf50ca3f4beb5b8f91
Karanjeet-kumar <karanrajput8400@gmail.com>
2024-08-25 03:02:19 +0530

    Loading Page Created

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import Search from "./components/Search";
 import Home from "./components/Home";
 import PlayingVideo from "./components/PlayingVideo";
+import Loading from "./loader/Loading";
+import { useAuth } from "./context/AuthProvider";
 
 function App() {
+  const {loading}=useAuth()
   return (
     <div>
+      {loading && <Loading />}
       <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
