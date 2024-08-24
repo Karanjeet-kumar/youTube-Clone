b6d3b9c0a6937d54f6e4d9a000767de13d0fc1f6
Karanjeet-kumar <karanrajput8400@gmail.com>
2024-08-25 02:52:25 +0530

    Homepage  updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,14 @@ import Video from "./Video"
 import { useAuth } from '../context/AuthProvider'
 
 function Home() {
-    const { data } = useAuth();
+    const { data, loading } = useAuth();
     console.log(data);
   return (
     <div className='flex justify-between mt-10'>
         <Sidebar />
         <div className='w-[100%] h-[calc(100vh-3.625rem)] overflow-y-scroll overflow-x-hidden '>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-x-3 p-2 gap-y-5'>
-                {data.map((item) => {
+                {!loading && data.map((item) => {
                     if (item.type !== "video") return false;
                     return <Video key={item.id} video={item?.video} />
                 })}
