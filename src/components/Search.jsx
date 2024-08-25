71f062aab02ef142d8ca9ec48e02035f2434a067
Karanjeet-kumar <karanrajput8400@gmail.com>
2024-08-25 17:16:24 +0530

    Search-Page Created

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom"
+import { fetchData } from "../utils/rapid_api"
+import Sidebar from "./Sidebar"
+import SearchCard from './SearchCard';
 
 function Search() {
+  const {result, setResult}=useState();
+  const {searchQuery}=useParams();
+
+  useEffect(()=>{
+    fetchSearchResults()
+  }, [searchQuery])
+
+  const fetchSearchResults =()=>{
+    fetchData(`search/?q=${searchQuery}`).then(({contents}) => {
+      console.log(contents);
+      setResult(contents);
+    })
+  } 
+
   return (
-    <div>Search</div>
+    <div>
+      <div className='mt-24 flex-row]'>
+        <Sidebar />
+        <div className='h-[calc(100vh-3.325rem)] overflow-y-scroll overflow-x-hidden'>
+          <div className='grid grid-col-1 gap-2 p-2'>
+            {result?.map((item, index)=>{
+              if(item?.type!=="video") return false;
+              return <SearchCard key={index} video={item?.video} />
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
